6823c9e4aa09ebf8a3ca63ce417a74a89500a783
Taylor Viti <tviti@hawaii.edu>
2019-11-13 23:11:19 -1000

    Initial commit.

---
+++ home.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+let home_directory = builtins.getEnv "HOME";
+    my-python-packages = python-packages: with python-packages; [
+      # Things for Nex
+      # TODO: Move to a seperate package?
+      pyqt5
+      pyqtwebengine
+    ];
+    python-with-my-packages = pkgs.python3.withPackages my-python-packages;
+in rec {
+  home = {
+    packages = [
+      pkgs.git
+      pkgs.aspell
+      pkgs.aspellDicts.en
+      pkgs.aspellDicts.en-computers
+      pkgs.aspellDicts.en-science
+      pkgs.pass
+
+      python-with-my-packages
+    ];
+
+    sessionVariables = {
+      ASPELL_CONF = "conf ${xdg.configHome}/aspell/config;";
+    };
+  };
+
+  home.file.".bash_profile".source = ./.bash_profile;
+  
+  programs.git = {
+    enable = true;
+	  userName = "Taylor Viti";
+	  userEmail = "tviti@hawaii.edu";
+  };
+
+  programs.gpg = {
+    enable = true;
+  };
+
+  xdg = {
+    enable = true;
+
+    configHome = "${home_directory}/.config";
+
+    configFile."aspell/config".text = ''
+      home-dir ${home_directory}/Sync;
+    '';
+  };
+  
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
