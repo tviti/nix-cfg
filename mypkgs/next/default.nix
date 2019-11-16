115a8da487f0b550e82875b183895e90a4c19276
Taylor Viti <tviti@hawaii.edu>
2019-11-16 02:23:39 -1000

    next-pyqt: Make derivation darwin only.

--- mypkgs/next/default.nix
+++ mypkgs/next/default.nix
@@ -5,13 +5,6 @@ in stdenv.mkDerivation rec {
   name = "next-pyqt";
   version = "9d65598a0911a09e3befcad65444763a51527913";
 
-  # src = fetchFromGitHub {
-  #   owner = "tviti";
-  #   repo = "next";
-  #   rev = "9d65598a0911a09e3befcad65444763a51527913";
-  #   sha256 = "1z51bkspw25q40d0myz45h14jxsjgdlcc0nx8bdz4hhg5k4b52ky";
-  # };
-
   src = fetchurl {
     url = "https://github.com/tviti/next/archive/${version}.tar.gz";
     sha256 = "0ba7i7yn6ya7qdc768z4z23x2zyq78w67pgs6abav2jyxp83qy2n";
@@ -30,5 +23,9 @@ in stdenv.mkDerivation rec {
   nativeBuildInputs =
     [ sbcl makeWrapper libfixposix ];
 
-  buildInputs =  [ python3  curl cacert dbus gcc ];
+  buildInputs =  [ python3  curl cacert dbus gcc gzip pass ];
+
+  meta = {
+    platforms = stdenv.lib.platforms.darwin;
+  };
 }
