6eb609491789a0e6fab7f3b071f5124d937eaa9b
Taylor Viti <tviti@hawaii.edu>
2019-11-16 02:22:49 -1000

    libfixposix: Switch to generic unpacker.

--- mypkgs/libfixposix/default.nix
+++ mypkgs/libfixposix/default.nix
@@ -1,26 +1,26 @@
+# This file is basically a transliteration of the brew recipe for libfixposix.
+# For some reason, the one in nixpkgs doesn't support darwin, so I rolled my own.
+
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
-  version = "0.4.3";
   name = "libfixposix";
+  version = "0.4.3";
+
   src = fetchurl {
     url = "https://github.com/sionescu/libfixposix/archive/v${version}.tar.gz";
     sha256 = "78fe8bcebf496520ac29b5b65049f5ec1977c6bd956640bdc6d1da6ea04d8504";
   };
 
-  buildPhase = ''
-    tar -xvzf ${src}
-    cd ./libfixposix-${version}
-    
+  configurePhase = ''
     autoreconf -fvi
     ./configure --disable-dependency-tracking \
                 --disable-silent-rules \
                 --prefix=$out
-    make 
   '';
+
+  buildInputs = [ autoconf automake libtool pkg-config git getconf ];
 
   installPhase = ''
     make install
   '';
-
-  buildInputs = [ autoconf automake libtool pkg-config git getconf ];
 }
