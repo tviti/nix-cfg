eb6c30f48f017a4db719f0f1ed6d0a81b6c404a2
Taylor Viti <tviti@hawaii.edu>
2019-11-23 04:35:01 -1000

    Revert "home: Moved curl and pass to system level packages."
    This reverts commit d215279ca6185eef95544d61a663ad8311d61df9.

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -35,9 +35,6 @@ in {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    vim
-    curl
-    pass
     skhd
     yabai
   ];
