677296584c2143b99f9222b64301000d863496be
Taylor Viti <tviti@hawaii.edu>
2019-12-22 15:09:10 -1000

    mother-night: Separated common code from home.nix

--- machines/mother-night/home.nix
+++ machines/mother-night/home.nix
@@ -1,102 +1,17 @@
 { config, pkgs, stdenv, ... }:
 
 let
-  home-dir = builtins.getEnv "HOME";
   pkg-dir = ../../pkgs;
-  config-dir = ../../config;
-  next = pkgs.callPackage (pkg-dir + "/next") {
-    libfixposix = if stdenv.isDarwin then
-      pkgs.callPackage (pkg-dir + "/libfixposix") { }
-    else
-      pkgs.libfixposix;
-  };
-  texlab = pkgs.callPackage (pkg-dir + "/texlab") { };
   kitty-themes = pkgs.callPackage (pkg-dir + "/kitty-themes") { };
 in rec {
-  home = {
-    packages = with pkgs;
-      [
-        aspell
-        aspellDicts.en
-        aspellDicts.en-computers
-        aspellDicts.en-science
-        # bibutils
-        curl
-        direnv
-        git
-        gitAndTools.git-annex
-        myEmacs
-        myR
-        next
-        nixfmt # for emacs nix-mode
-        pandoc
-        pv
-        rclone
-        spotify
-        haskellPackages.pandoc-citeproc
-        #pass
-        wget
-        vim
-        # texlab # LSP server for latex
-        # lua53Packages.digestif # LSP server for latex
-        kitty-themes
-      ] ++ myMappingTools;
+  imports = [
+    ../../home-common.nix
+  ];
 
-    sessionVariables = {
-      ASPELL_CONF = "conf ${xdg.configHome}/aspell/config;";
-      ALTERNATE_EDITOR = "${pkgs.vim}/bin/vi";
-      EDITOR = "${pkgs.myEmacs}/bin/emacsclient";
-    };
-  };
+  home.packages = [ kitty-themes ];
 
-  programs = {
-    bash = {
-      enable = true;
-      profileExtra = ''
-        # export PATH=$PATH:/Applications/MATLAB_R2016a.app/bin/
-        # source ${home-dir}/.nix-profile/etc/profile.d/nix.sh
-      '';
-    };
-
-    git = {
-      enable = true;
-      userName = "Taylor Viti";
-      userEmail = "tviti@hawaii.edu";
-    };
-
-    # gpg.enable = true;
-  };
-
-  xdg = {
-    enable = true;
-
-    # Configuration files
-    configHome = "${home-dir}/.config";
-
-    configFile."next".source = config-dir + "/next-cfg";
-    configFile."i3".source = config-dir + "/i3";
-
-    configFile."aspell/config".text = ''
-      dict-dir ${home-dir}/.nix-profile/lib/aspell
-      home-dir ${home-dir}/Sync
-      personal ${home-dir}/Sync/.aspell.en.pws
-
-      master en_US
-      extra-dicts en_US-science.rws
-      add-extra-dicts en-computers.rws
-    '';
-
-    configFile."kitty/kitty.conf".text = ''
-      # Load a theme
-      include ${kitty-themes}/share/kitty-themes/gruvbox_light.conf
-    '';
-  };
-
-  home.file.".chktexrc".source = config-dir + "/chktexrc";
-
-  services.syncthing.enable = true;
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
+  xdg.configFile."kitty/kitty.conf".text = ''
+    # Load a theme
+    include ${kitty-themes}/share/kitty-themes/gruvbox_light.conf
+  '';
 }
