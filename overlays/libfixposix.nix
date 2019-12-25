869b3fcebc7cb8dbedef55c1b50c26639c51cd68
Taylor Viti <tviti@hawaii.edu>
2019-12-24 23:56:44 -1000

    overlays: Corrected refs to callPackage and libfixposix.

--- overlays/libfixposix.nix
+++ overlays/libfixposix.nix
@@ -3,7 +3,7 @@ self: super:
 let pkg-dir = ../pkgs;
 in {
   libfixposix = if super.stdenv.isDarwin then
-    super.pkgs.callPackage (pkg-dir + "/libfixposix") { }
+    super.callPackage (pkg-dir + "/libfixposix") { }
   else
-    super.pkgs.libfixposix;
+    super.libfixposix;
 }
