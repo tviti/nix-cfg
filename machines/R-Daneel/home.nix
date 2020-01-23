e09317c12c3c95cde3273a99fe2191c783405b90
Taylor Viti <tviti@hawaii.edu>
2020-01-22 18:45:31 -1000

    R-Daneel: Added conda shell setup block back into ~/.profile.
    Needed to use NCL today, and the quickest way to install it turns out
    to be through conda (this is actually the preferred way to install it,
    per the official instructions). To "re-enable" my existing conda
    install, I had to add this conda-shell setup chunk back into my bash env.

--- machines/R-Daneel/home.nix
+++ machines/R-Daneel/home.nix
@@ -21,6 +21,24 @@ in rec {
       profileExtra = ''
         export PATH=$PATH:/Applications/MATLAB_R2016a.app/bin/
         source ${home_directory}/.nix-profile/etc/profile.d/nix.sh
+
+        # added by Anaconda2 5.0.0 installer
+        # export PATH="/Users/taylor/anaconda2/bin:$PATH"  # commented out by conda initialize
+
+        # >>> conda initialize >>>
+        # !! Contents within this block are managed by 'conda init' !!
+        __conda_setup="$('/Users/taylor/anaconda2/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+        if [ $? -eq 0 ]; then
+            eval "$__conda_setup"
+        else
+            if [ -f "/Users/taylor/anaconda2/etc/profile.d/conda.sh" ]; then
+                . "/Users/taylor/anaconda2/etc/profile.d/conda.sh"
+            else
+                export PATH="/Users/taylor/anaconda2/bin:$PATH"
+            fi
+        fi
+        unset __conda_setup
+        # <<< conda initialize <<<
       '';
     };
 
