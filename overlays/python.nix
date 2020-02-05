d9e54e70d34e4cb9ae9c7e16ee865adc98d7d6a6
Taylor Viti <tviti@hawaii.edu>
2020-02-05 00:34:42 -1000

    overlays: Added pyls to base python env.

--- overlays/python.nix
+++ overlays/python.nix
@@ -1,6 +1,7 @@
 self: super: {
   myPython3Packages = pythonPackages: with pythonPackages; [
     ipython
+    python-language-server
   ];
   myPython3 = super.python3.withPackages super.pkgs.myPython3Packages;
 }
