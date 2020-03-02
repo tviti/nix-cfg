e814573b6e1a8f0a5860e3a5cb3735ce7dcb1cdf
Taylor Viti <tviti@hawaii.edu>
2020-03-02 01:54:57 -1000

    pkgs/matlab: Trying out different set of targetPkgs.
    targetPkgs taken from https://github.com/jorsn/matlab-nix-imperative/blob/master/default.nix
    
    With this setup, I can launch the MATLAB desktop, but it segfaults
    after a few seconds.

--- pkgs/matlab/default.nix
+++ pkgs/matlab/default.nix
@@ -6,48 +6,31 @@ let
 in buildFHSUserEnv {
   name = "matlab";
 
-  targetPkgs = pkgs:
-    (with pkgs; [
-      udev
-      coreutils
-      alsaLib
-      dpkg
-      gcc6
-      zlib
-      freetype
-      glib
-      zlib
-      fontconfig
-      openssl
-      which
-      ncurses
-      jdk11
-      pam
-      dbus_glib
-      dbus
-      pango
-      gtk2-x11
-      atk
-      gdk_pixbuf
-      cairo
-    ]) ++ (with pkgs.xorg; [
-      libX11
-      libXcursor
-      libXrandr
-      libXext
-      libSM
-      libICE
-      libXdamage
-      libXrender
-      libXfixes
-      libXcomposite
-      libxcb
-      libXi
-      libXScrnSaver
-      libXtst
-      libXt
-      libXxf86vm
-    ]);
+  targetPkgs = pkgs: with pkgs; [
+    glib
+    libGL
+    jre
+    mesa_glu
+    ncurses
+    pam
+    zlib
+  ]
+  ++
+  (with xorg; [
+    libX11
+    libXft
+    libXext
+    libXi
+    libXmu
+    libXp
+    libXpm
+    libXrandr
+    libXrender
+    libXt
+    libXtst
+    libXxf86vm
+    libxcb
+  ]);
 
   # runScript = "bash";
   runScript = "${runPath}/bin/matlab";
