ea1992cb2ca2047e6f8d6851718d488280820f42
Taylor Viti <tviti@hawaii.edu>
2020-03-07 16:51:08 -1000

    R-Daneel: Commented out anaconda setup chunk from profileExtra.
    Turns out that anaconda will "leak dependencies" into impure
    nix-shells that use python-language-server (the rest of the python env
    seems OK though), causing python-language-server to be unaware of
    python packages installed from nix.

--- machines/R-Daneel/home.nix
+++ machines/R-Daneel/home.nix
@@ -27,17 +27,17 @@ in rec {
 
         # >>> conda initialize >>>
         # !! Contents within this block are managed by 'conda init' !!
-        __conda_setup="$('/Users/taylor/anaconda2/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-        if [ $? -eq 0 ]; then
-            eval "$__conda_setup"
-        else
-            if [ -f "/Users/taylor/anaconda2/etc/profile.d/conda.sh" ]; then
-                . "/Users/taylor/anaconda2/etc/profile.d/conda.sh"
-            else
-                export PATH="/Users/taylor/anaconda2/bin:$PATH"
-            fi
-        fi
-        unset __conda_setup
+        # __conda_setup="$('/Users/taylor/anaconda2/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+        # if [ $? -eq 0 ]; then
+        #     eval "$__conda_setup"
+        # else
+        #     if [ -f "/Users/taylor/anaconda2/etc/profile.d/conda.sh" ]; then
+        #         . "/Users/taylor/anaconda2/etc/profile.d/conda.sh"
+        #     else
+        #         export PATH="/Users/taylor/anaconda2/bin:$PATH"
+        #     fi
+        # fi
+        # unset __conda_setup
         # <<< conda initialize <<<
       '';
     };
