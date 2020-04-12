9863282314a24e9963c7e1828bbc631eca91a7ec
Taylor Viti <tviti@hawaii.edu>
2020-04-11 19:15:26 -1000

    mother-night: Added package ncview + config.
    The configuration file (.ncviewrc) is basically the default produced
    by the first run of ncview, but with the blu_red colormap set as the default.

--- machines/mother-night/home.nix
+++ machines/mother-night/home.nix
@@ -18,6 +18,7 @@ in rec {
     skypeforlinux
     spotify
     mu
+    ncview
   ]);
 
   xdg.configFile."kitty/kitty.conf".text = ''
@@ -25,6 +26,31 @@ in rec {
     include ${kitty-themes}/share/kitty-themes/gruvbox_light.conf
   '';
 
+  home.file."/.ncviewrc".text = ''
+    -1 "STRINGLIST_SAVE_FILE_VERSION" INT 1
+    0 "NCVIEW_STATE_FILE_VERSION" INT 1
+    1 "CMAP_blu_red" INT 1
+    2 "CMAP_detail" INT 1
+    3 "CMAP_ssec" INT 1
+    4 "CMAP_bright" INT 1
+    5 "CMAP_banded" INT 1
+    6 "CMAP_rainbow" INT 1
+    7 "CMAP_jaisnb" INT 1
+    8 "CMAP_jaisnc" INT 1
+    9 "CMAP_jaisnd" INT 1
+    10 "CMAP_3gauss" INT 1 
+    11 "CMAP_manga" INT 1
+    12 "CMAP_jet" INT 1
+    13 "CMAP_wheel" INT 1
+    14 "CMAP_3saw" INT 1
+    15 "CMAP_bw" INT 1
+    16 "CMAP_default" INT 1
+    17 "CMAP_extrema" INT 1
+    18 "CMAP_helix" INT 1
+    19 "CMAP_helix2" INT 1
+    20 "CMAP_hotres" INT 1
+  '';
+  
   services.syncthing.enable = true;
 
   #
