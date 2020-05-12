852caf963f94dd4dcc9954be1793107bae024f43
Taylor Viti <tviti@hawaii.edu>
2020-05-11 15:04:44 -1000

    pkgs/idv on aunuu: Undid fix for opengl on nouveu driver.
    I'm using the proprietary nvidia drivers on this machine, so this line
    is no longer necessary.

--- pkgs/idv/default.nix
+++ pkgs/idv/default.nix
@@ -24,7 +24,6 @@ in stdenv.mkDerivation rec {
     cp *.jar $out/share/idv
 
     makeWrapper "${jre}/bin/java" "$out/bin/idv" \
-      --set MESA_LOADER_DRIVER_OVERRIDE i965 \
       --add-flags "-Xmx512m -cp $CLASSPATH:$out/share/idv/auxdata.jar:$out/share/idv/external.jar:$out/share/idv/jython.jar:$out/share/idv/local-visad.jar:$out/share/idv/ncIdv.jar:$out/share/idv/visad.jar:$out/share/idv/idv.jar ucar.unidata.idv.DefaultIdv"
   '';
 }
