f65989fec6416aeed0139f0cadf26a74b1414da3
Taylor Viti <tviti@hawaii.edu>
2020-07-16 00:43:15 -1000

    Revert "aunuu: Update MATLAB drv for R2020a."
    This reverts commit fe6d102ae70ecdced669765634e0c9faffe67281.

--- pkgs/matlab/default.nix
+++ pkgs/matlab/default.nix
@@ -1,8 +1,6 @@
 { callPackage }:
 
-# TODO: Be explicit about versions in file and object names!
-let
-  common = import ./common.nix { };
+let common = import ./common.nix { };
 in {
   matlab = callPackage ./matlab.nix { inherit common; };
   mlint = callPackage ./mlint.nix { inherit common; };
