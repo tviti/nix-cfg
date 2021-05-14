c9ecdb29af7022f3e43fae7e0061519f74bed1dd
Taylor Viti <tviti@hawaii.edu>
2021-05-14 11:27:21 -1000

    Don't override qgis version
    Latest nixpkgs checkout has a new-enough version

--- overlays/qgis.nix
+++ overlays/qgis.nix
@@ -1,14 +1,4 @@
 self: super: {
   qgis-unwrapped = super.qgis-unwrapped.overrideAttrs (oldAttrs: rec {
-    version = "3.14.15";
-
-    src = super.fetchFromGitHub {
-      owner = "qgis";
-      repo = "QGIS";
-      rev = "final-${super.lib.replaceStrings ["."] ["_"] version}";
-      sha256 = "03skj8dd5rqi5k22zi8bly1sbynywm2bv6184dhjvw4i2vv4p3rl";
-    };
-
-    buildInputs = oldAttrs.buildInputs ++ [ super.protobuf ];
   });
 }
