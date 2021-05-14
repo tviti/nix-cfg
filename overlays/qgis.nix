4a54b75ac97f70d538a9528b445f4892985187f7
Taylor Viti <tviti@hawaii.edu>
2021-05-14 11:28:26 -1000

    Build qgis w/ qt3d enabled
    Needed by Crayfish plugin

--- overlays/qgis.nix
+++ overlays/qgis.nix
@@ -1,4 +1,6 @@
 self: super: {
   qgis-unwrapped = super.qgis-unwrapped.overrideAttrs (oldAttrs: rec {
+    buildInputs = oldAttrs.buildInputs ++ [ super.qt5.qt3d ];
+    cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DWITH_3D=ON" ];
   });
 }
